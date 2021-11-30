273898bfa56272e90c09faccb8a289896716383c
Ejaz <ejaz345@hotmail.com>
2021-11-30 22:48:20 +0000

    debugging

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ export default () => {
   const decalTextureName = "bulletHole.jpg";
   const decalTexture = textureLoader.load(`${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}${ decalTextureName}`);
   decalTexture.needsUpdate = true;
+  decalTexture.repeat.set(4,4);
   decalTexture.wrapS = RepeatWrapping;
   decalTexture.wrapT = RepeatWrapping;
   const decalMaterial = new THREE.MeshPhysicalMaterial({map:decalTexture, alphaMap: decalTexture, transparent: true, depthWrite: true, depthTest: true});
