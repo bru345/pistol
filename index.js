0982db3b3afcf3c90c72dcdc194424be55a5eb28
Ejaz <ejaz345@hotmail.com>
2021-12-01 22:35:29 +0000

    debugging

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ export default () => {
             ));
 
             planeGeo.applyMatrix4(plane.matrixWorld)
-            planeGeo.applyQuaternion(plane.getWorldQuaternion())
+            planeGeo.applyQuaternion(plane.quaternion)
             
             //confirming if issue is positional/rotation
             //REMOVE THIS. RESOLVE MATRIX FOR GEOMETRYBUFFER ELSEWHERE
